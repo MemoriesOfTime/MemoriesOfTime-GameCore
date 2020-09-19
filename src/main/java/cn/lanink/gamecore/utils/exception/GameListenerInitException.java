d6b41a2afd8543dc1ce95a2a950567e7f437cd91
lt-name <572413378@qq.com>
2020-09-19 15:47:04 +0800

    Stuff

---
+++ src/main/java/cn/lanink/gamecore/utils/exception/GameListenerInitException.java
@@ -0,0 +1,16 @@
+package cn.lanink.gamecore.utils.exception;
+
+/**
+ * @author lt_name
+ */
+public class GameListenerInitException extends Exception {
+
+    public GameListenerInitException() {
+
+    }
+
+    public GameListenerInitException(String message) {
+        super(message);
+    }
+
+}
