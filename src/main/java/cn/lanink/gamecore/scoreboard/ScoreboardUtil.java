c450e573288bb48589561d9a96cf12949733d34e
lt-name <572413378@qq.com>
2020-09-30 11:03:09 +0800

    检查依赖是否加载

--- src/main/java/cn/lanink/gamecore/scoreboard/ScoreboardUtil.java
+++ src/main/java/cn/lanink/gamecore/scoreboard/ScoreboardUtil.java
@@ -2,6 +2,7 @@ package cn.lanink.gamecore.scoreboard;
 
 import cn.lanink.gamecore.api.Info;
 import cn.lanink.gamecore.scoreboard.base.IScoreboard;
+import cn.nukkit.Server;
 
 /**
  * @author lt_name
@@ -19,10 +20,16 @@ public class ScoreboardUtil {
         if (scoreboard == null) {
             try {
                 Class.forName("gt.creeperface.nukkit.scoreboardapi.ScoreboardAPI");
+                if (Server.getInstance().getPluginManager().getPlugin("ScoreboardAPI").isDisabled()) {
+                    throw new Exception("Not Loaded");
+                }
                 scoreboard = new cn.lanink.gamecore.scoreboard.creeperface.SimpleScoreboard();
             } catch (Exception e) {
                 try {
                     Class.forName("de.theamychan.scoreboard.ScoreboardPlugin");
+                    if (Server.getInstance().getPluginManager().getPlugin("ScoreboardPlugin").isDisabled()) {
+                        throw new Exception("Not Loaded");
+                    }
                     scoreboard = new cn.lanink.gamecore.scoreboard.theamychan.SimpleScoreboard();
                 } catch (Exception e1) {
                     scoreboard = new cn.lanink.gamecore.scoreboard.ltname.SimpleScoreboard();
