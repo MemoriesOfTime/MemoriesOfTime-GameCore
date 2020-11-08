de258e4c96db0039ca5ced824b16223ee86fded1
lt-name <572413378@qq.com>
2020-11-08 21:37:11 +0800

    Stuff

--- src/main/java/cn/lanink/gamecore/room/IRoomStatus.java
+++ src/main/java/cn/lanink/gamecore/room/IRoomStatus.java
@@ -1,15 +1,22 @@
 package cn.lanink.gamecore.room;
 
+import cn.lanink.gamecore.api.Info;
+
 /**
  * @author lt_name
  */
 public interface IRoomStatus {
 
-    int ROOM_STATUS_LEVEL_NOT_LOADED = -1; //地图未加载
-    int ROOM_STATUS_TASK_NEED_INITIALIZED = 0; //Task未初始化
-    int ROOM_STATUS_WAIT = 1; //等待玩家加入
-    int ROOM_STATUS_GAME = 2; //游戏中
-    int ROOM_STATUS_VICTORY = 3; //胜利结算中
+    @Info("地图未加载")
+    int ROOM_STATUS_LEVEL_NOT_LOADED = -1;
+    @Info("Task未初始化")
+    int ROOM_STATUS_TASK_NEED_INITIALIZED = 0;
+    @Info("等待玩家加入")
+    int ROOM_STATUS_WAIT = 1;
+    @Info("游戏中")
+    int ROOM_STATUS_GAME = 2;
+    @Info("胜利结算中")
+    int ROOM_STATUS_VICTORY = 3;
 
     /**
      * @param status 房间状态
