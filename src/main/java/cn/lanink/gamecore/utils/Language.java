7178ab2062703ee66aae7e22ead3e37f9da8c2af
lt_name <572413378@qq.com>
2021-01-05 12:35:44 +0800

    Stuff

--- src/main/java/cn/lanink/gamecore/utils/Language.java
+++ src/main/java/cn/lanink/gamecore/utils/Language.java
@@ -32,13 +32,40 @@ public class Language {
 
     public String translateString(String key, Object... params) {
         String string = this.cache.computeIfAbsent(key,
-                str -> this.config.getString(key, "§c Language reading error! "));
+                str -> this.config.getString(key, "§c Language reading error!"));
         if (params != null && params.length > 0) {
             for (int i = 1; i < params.length + 1; i++) {
-                string = string.replace("%" + i, Objects.toString(params[i-1]));
+                string = string.replace("%" + i + "%", Objects.toString(params[i-1]));
             }
         }
         return string;
+    }
+
+    public void update(File newFile) {
+        this.update(newFile, Config.PROPERTIES);
+    }
+
+    public void update(File newFile, int type) {
+        this.update(new Config(newFile, type));
+    }
+
+    public void update(Config newConfig) {
+        for (String key : this.config.getKeys()) {
+            if (newConfig.getKeys().contains(key)) {
+                this.cache.put(key, this.config.getString(key, "§c Language reading error!"));
+            }else {
+                this.cache.remove(key);
+                this.config.remove(key);
+            }
+        }
+        for (String key : newConfig.getKeys()) {
+            if (!this.cache.containsKey(key)) {
+                String string = newConfig.getString(key, "§c Language reading error!");
+                this.config.set(key, string);
+                this.cache.put(key, string);
+            }
+        }
+        this.config.save(true);
     }
 
 
