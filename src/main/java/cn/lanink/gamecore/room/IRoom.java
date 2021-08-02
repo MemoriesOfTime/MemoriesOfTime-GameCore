da2c27f7995f6d86a55e4c61236fae51459fa69e
lt_name <572413378@qq.com>
2021-08-02 09:54:47 +0800

    Stuff

--- src/main/java/cn/lanink/gamecore/room/IRoom.java
+++ src/main/java/cn/lanink/gamecore/room/IRoom.java
@@ -1,15 +1,17 @@
 package cn.lanink.gamecore.room;
 
+import cn.nukkit.level.Level;
+
 /**
  * @author lt_name
  */
 public interface IRoom {
 
-/*    Level getLevel();
+    Level getLevel();
 
     String getLevelName();
 
-    void joinRoom(Player player, boolean spectator);
+    /*void joinRoom(Player player, boolean spectator);
 
     void quitRoom(Player player);
 
