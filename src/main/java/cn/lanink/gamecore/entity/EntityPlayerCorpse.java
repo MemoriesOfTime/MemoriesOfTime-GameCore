7ea83a9f34e4e0e3031f66edd7ff6cde47bdfc69
lt_name <572413378@qq.com>
2021-08-25 17:45:20 +0800

    Stuff

--- src/main/java/cn/lanink/gamecore/entity/EntityPlayerCorpse.java
+++ src/main/java/cn/lanink/gamecore/entity/EntityPlayerCorpse.java
@@ -1,11 +1,32 @@
 package cn.lanink.gamecore.entity;
 
+import cn.lanink.gamecore.GameCore;
+import cn.nukkit.entity.Entity;
 import cn.nukkit.entity.EntityHuman;
+import cn.nukkit.entity.data.Skin;
+import cn.nukkit.level.Position;
 import cn.nukkit.level.format.FullChunk;
 import cn.nukkit.nbt.tag.CompoundTag;
 
 @SuppressWarnings("unused")
 public class EntityPlayerCorpse extends EntityHuman {
+
+    public static EntityPlayerCorpse createPlayerCorpse(Position position) {
+        return createPlayerCorpse(position, GameCore.DEFAULT_SKIN);
+    }
+
+    public static EntityPlayerCorpse createPlayerCorpse(Position position, Skin skin) {
+        EntityPlayerCorpse entityPlayerCorpse = new EntityPlayerCorpse(
+                position.getChunk(),
+                Entity.getDefaultNBT(position)
+                        .putCompound("Skin", new CompoundTag()
+                                .putByteArray("Data", skin.getSkinData().data)
+                                .putString("ModelId", skin.getSkinId())
+                        )
+        );
+        entityPlayerCorpse.setSkin(skin);
+        return entityPlayerCorpse;
+    }
 
     public EntityPlayerCorpse(FullChunk chunk, CompoundTag nbt) {
         super(chunk, nbt);
