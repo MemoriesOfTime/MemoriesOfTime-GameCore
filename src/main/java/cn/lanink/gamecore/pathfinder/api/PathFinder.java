fdca3fa93cf69ec9a2710c846718da6263bda9c1
LT_Name <45508179+lt-name@users.noreply.github.com>
2021-08-25 04:35:18 -0500

    寻路 (#11)

---
+++ src/main/java/cn/lanink/gamecore/pathfinder/api/PathFinder.java
@@ -0,0 +1,36 @@
+package cn.lanink.gamecore.pathfinder.api;
+
+import cn.lanink.gamecore.api.Info;
+import cn.nukkit.level.particle.Particle;
+import cn.nukkit.math.Vector3;
+
+import java.util.List;
+
+/**
+ * @author iGxnon
+ * @date  2021/08/24
+ */
+public interface PathFinder {
+
+    /**
+     * @return 返回下一个路径节点 | 没有路径返回 null
+     */
+    Vector3 findNext();
+
+    /**
+     * @return 返回所有路径节点 | 没有路径返回 null
+     */
+    List<Vector3> find();
+
+    /**
+     * @param async 是否异步
+     * @return 返回所有路径节点 | 没有路径返回 null
+     */
+    List<Vector3> find(boolean async);
+
+    /**
+     * 展示路径 用于测试
+     */
+    void show();
+
+}
