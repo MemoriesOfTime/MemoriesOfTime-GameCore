9675f159f8f987f391d106ca0efa6131bc5857d7
iGxnon <1545766400@qq.com>
2021-09-11 11:51:00 +0800

    stuff

--- src/main/java/cn/lanink/gamecore/GameCore.java
+++ src/main/java/cn/lanink/gamecore/GameCore.java
@@ -14,7 +14,7 @@ import java.util.Base64;
 public class GameCore extends PluginBase {
 
     public static final Gson GSON = new Gson();
-    public static final String VERSION = "1.3.5-SNAPSHOT git-a72d29a";
+    public static final String VERSION = "1.4.0-SNAPSHOT git-6a6dc4e";
 
     private static GameCore gameCore;
 
