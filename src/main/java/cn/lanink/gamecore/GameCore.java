65685a3e619d0d59b7fd9e0ff5fb9f3b50950785
LT_Name <572413378@qq.com>
2021-09-14 09:32:11 +0800

    Stuff

--- src/main/java/cn/lanink/gamecore/GameCore.java
+++ src/main/java/cn/lanink/gamecore/GameCore.java
@@ -14,7 +14,7 @@ import java.util.Base64;
 public class GameCore extends PluginBase {
 
     public static final Gson GSON = new Gson();
-    public static final String VERSION = "1.3.0-SNAPSHOT git-11d0dd1";
+    public static final String VERSION = "?";
 
     private static GameCore gameCore;
 
