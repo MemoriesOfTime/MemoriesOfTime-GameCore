3cc27f545b223a16debebbf0255ffc2cc5d443ad
LT_Name <572413378@qq.com>
2021-09-17 22:58:26 +0800

    Stuff

--- src/main/java/cn/lanink/gamecore/form/WindowListener.java
+++ src/main/java/cn/lanink/gamecore/form/WindowListener.java
@@ -1,16 +1,25 @@
 package cn.lanink.gamecore.form;
 
-import cn.lanink.gamecore.form.inventory.advanced.AdvancedEntityInventory;
+import cn.lanink.gamecore.GameCore;
 import cn.lanink.gamecore.form.inventory.advanced.AdvancedInventory;
 import cn.lanink.gamecore.form.windows.AdvancedFormWindowCustom;
 import cn.lanink.gamecore.form.windows.AdvancedFormWindowModal;
 import cn.lanink.gamecore.form.windows.AdvancedFormWindowSimple;
+import cn.nukkit.Player;
+import cn.nukkit.Server;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.inventory.InventoryClickEvent;
 import cn.nukkit.event.inventory.InventoryCloseEvent;
 import cn.nukkit.event.player.PlayerFormRespondedEvent;
+import cn.nukkit.event.player.PlayerServerSettingsRequestEvent;
+import cn.nukkit.event.player.PlayerSettingsRespondedEvent;
+import cn.nukkit.form.window.FormWindow;
+import cn.nukkit.network.protocol.ServerSettingsResponsePacket;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author lt_name
@@ -27,6 +36,27 @@ public class WindowListener implements Listener {
             return;
         }
         AdvancedFormWindowCustom.onEvent(event.getWindow(), event.getPlayer());
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onPlayerSettingsResponded(PlayerSettingsRespondedEvent event) {
+        AdvancedFormWindowCustom.onEvent(event.getWindow(), event.getPlayer());
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onSettingsRequest(PlayerServerSettingsRequestEvent event) {
+        Player player = event.getPlayer();
+        HashMap<Integer, FormWindow> map = new HashMap<>(event.getSettings());
+        event.setSettings(new HashMap<>());
+        //必须延迟一下，否则客户端不显示
+        Server.getInstance().getScheduler().scheduleDelayedTask(GameCore.getInstance(), () -> {
+            for (Map.Entry<Integer, FormWindow> entry : map.entrySet()) {
+                ServerSettingsResponsePacket pk = new ServerSettingsResponsePacket();
+                pk.formId = entry.getKey();
+                pk.data = entry.getValue().getJSONData();
+                player.dataPacket(pk);
+            }
+        }, 20);
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
