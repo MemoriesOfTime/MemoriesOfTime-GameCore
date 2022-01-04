6f6ab425648fbd92ce868bbae99b0a8e9666a737
iGxnon <1545766400@qq.com>
2022-01-05 00:04:04 +0800

    添加更多查询方法

--- src/main/java/cn/lanink/gamecore/utils/ItemInfoUtils.java
+++ src/main/java/cn/lanink/gamecore/utils/ItemInfoUtils.java
@@ -1,18 +1,21 @@
 package cn.lanink.gamecore.utils;
 
 import cn.lanink.gamecore.GameCore;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import lombok.Data;
+import lombok.NonNull;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author LT_Name
@@ -49,6 +52,38 @@ public class ItemInfoUtils {
         throw new RuntimeException("error");
     }
 
+    public static boolean registerCustomItemsFromMaps(@NonNull List<Map<String, Object>> maps) {
+        for (Map<String, Object> map : maps) {
+            if (!registerCustomItemFromMap(map)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 注册 CustomItem
+     */
+    public static void registerCustomItem(int id, int damage, String imagePath, String nameChinese, String nameEnglish) {
+        ITEM_INFOS.add(new ItemInfo(
+                id,
+                damage,
+                imagePath,
+                nameChinese,
+                nameEnglish
+        ));
+    }
+
+    public static boolean registerCustomItemFromMap(@NonNull Map<String, Object> map) {
+        try {
+            ItemInfo itemInfo = ItemInfo.fromMap(map);
+            ITEM_INFOS.add(itemInfo);
+        }catch (Exception ignored) {
+            return false;
+        }
+        return true;
+    }
+
     //TODO 添加更多查询方法
     public static ItemInfo getItemInfoById(int id) {
         return getItemInfoById(id, 0);
@@ -71,17 +106,52 @@ public class ItemInfoUtils {
         return getItemInfoById(id, damage).getNameChinese();
     }
 
+    public static String getNameEnglishById(int id) {
+        return getNameEnglishById(id, 0);
+    }
+
+    public static String getNameEnglishById(int id, int damage) {
+        return getItemInfoById(id, damage).getNameEnglish();
+    }
+
+    public static String getImgPathById(int id, int damage) {
+        return getItemInfoById(id, damage).getImagePath();
+    }
+
+    public static List<Integer> getAllMetaDamagesFromAnId(int id) {
+        List<Integer> ret = new LinkedList<>();
+        for (ItemInfo itemInfo : ITEM_INFOS) {
+            if (itemInfo.id == id) {
+                ret.add(itemInfo.damage);
+            }
+        }
+        return ret;
+    }
+
 
     @Data
     public static class ItemInfo {
 
         private final int id; //物品id
         private final int damage; //物品特殊值
+
         private final String imagePath; //贴图路径
         private final String nameChinese; //物品名称（中文）
         private final String nameEnglish; //物品名称（英文）
 
-        public static ItemInfo fromMap(Map<String, Object> map) {
+        private static final ImmutableList<String> availableKeys = new ImmutableList.Builder<String>()
+                .add("id")
+                .add("damage")
+                .add("imagePath")
+                .add("nameChinese")
+                .add("nameEnglish")
+                .build();
+
+        public static ItemInfo fromMap(@NonNull Map<String, Object> map) {
+            if (map.size() != availableKeys.size()) throw new RuntimeException("unexpected arguments in formMap(), expect 5 elements in map");
+            map.keySet().forEach(key -> {
+                if (!availableKeys.contains(key)) throw new RuntimeException("unexpected arguments in formMap(), expect " + availableKeys + " in map");
+            });
             return new ItemInfo(
                     (Integer) map.get("id"),
                     (Integer) map.get("damage"),
