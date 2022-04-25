6ad42daa085db34d6e0e672939cabb26cd0d0ae4
LT_Name <572413378@qq.com>
2022-04-25 21:22:39 +0800

    临时修复后缀报错问题

--- src/main/java/cn/lanink/gamecore/utils/VersionUtils.java
+++ src/main/java/cn/lanink/gamecore/utils/VersionUtils.java
@@ -1,7 +1,11 @@
 package cn.lanink.gamecore.utils;
 
+import cn.lanink.gamecore.GameCore;
 import cn.nukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.LinkedList;
 
 /**
  * @author LT_Name
@@ -23,33 +27,65 @@ public class VersionUtils {
         if (v1.equalsIgnoreCase(v2)) {
             return 0;
         }
-        String[] version1Array = v1.split("[._]");
-        String[] version2Array = v2.split("[._]");
-        int index = 0;
-        int minLen = Math.min(version1Array.length, version2Array.length);
-        long diff = 0;
+        try {
+            LinkedList<String> version1Array = new LinkedList<>(Arrays.asList(v1.split("[._-]")));
+            LinkedList<String> version2Array = new LinkedList<>(Arrays.asList(v2.split("[._-]")));
 
-        while (index < minLen
-                && (diff = Long.parseLong(version1Array[index])
-                - Long.parseLong(version2Array[index])) == 0) {
-            index++;
-        }
-        if (diff == 0) {
-            for (int i = index; i < version1Array.length; i++) {
-                if (Long.parseLong(version1Array[i]) > 0) {
-                    return 1;
+            LinkedList<String> version1Suffix = new LinkedList<>();
+            LinkedList<String> version2Suffix = new LinkedList<>();
+
+            int v1index = version1Array.size() - 1;
+            while (v1index != 0) {
+                try {
+                    Long.parseLong(version1Array.getLast());
+                }catch (Exception e) {
+                    version1Suffix.addLast(version1Array.pollLast());
                 }
+                v1index--;
+            }
+            int v2index = version2Array.size() - 1;
+            while (v2index != 0) {
+                try {
+                    Long.parseLong(version2Array.getLast());
+                }catch (Exception e) {
+                    version2Suffix.addLast(version2Array.pollLast());
+                }
+                v2index--;
             }
 
-            for (int i = index; i < version2Array.length; i++) {
-                if (Long.parseLong(version2Array[i]) > 0) {
-                    return -1;
-                }
+            int index = 0;
+            int minLen = Math.min(version1Array.size(), version2Array.size());
+            long diff = 0;
+
+            while (index < minLen
+                    && (diff = Long.parseLong(version1Array.get(index))
+                    - Long.parseLong(version2Array.get(index))) == 0) {
+                index++;
             }
-            return 0;
-        } else {
-            return diff > 0 ? 1 : -1;
+            if (diff == 0) {
+                //TODO 根据后缀判断
+
+                for (int i = index; i < version1Array.size(); i++) {
+                    if (Long.parseLong(version1Array.get(i)) > 0) {
+                        return 1;
+                    }
+                }
+
+                for (int i = index; i < version2Array.size(); i++) {
+                    if (Long.parseLong(version2Array.get(i)) > 0) {
+                        return -1;
+                    }
+                }
+                return 0;
+            } else {
+                return diff > 0 ? 1 : -1;
+            }
+        }catch (Exception e) {
+            if (GameCore.debug) {
+                GameCore.getInstance().getLogger().error("VersionUtils#compareVersion()", e);
+            }
         }
+        return -1;
     }
 
 }
