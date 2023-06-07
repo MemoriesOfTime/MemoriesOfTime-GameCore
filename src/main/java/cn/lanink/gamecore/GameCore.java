ae75dac62898d201fd30e8f1358abc53c9d8ee33
LT_Name <572413378@qq.com>
2023-06-07 22:09:59 +0800

    修复核心类型检查

--- src/main/java/cn/lanink/gamecore/GameCore.java
+++ src/main/java/cn/lanink/gamecore/GameCore.java
@@ -65,7 +65,7 @@ public class GameCore extends PluginBase {
     @Override
     public void onEnable() {
         NukkitTypeUtils.NukkitType nukkitType = NukkitTypeUtils.getNukkitType();
-        if (nukkitType != NukkitTypeUtils.NukkitType.PM1E) {
+        if (nukkitType != NukkitTypeUtils.NukkitType.MOT && nukkitType != NukkitTypeUtils.NukkitType.PM1E) {
             this.getLogger().warning("Warning! The current plugin version is not applicable to this server core! Please check the plugin version!");
             this.getLogger().warning("Server Code : " + nukkitType.getShowName() + "  |  Plugin Version : " + this.getVersion());
             try {
